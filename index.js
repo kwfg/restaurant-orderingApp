f593a1cd21f02b1d2ca46f247c840d91ced874c8
garyfung221 <garyfung266@gmail.com>
2023-03-03 22:37:34 +0800

    update

--- index.js
+++ index.js
@@ -3,15 +3,46 @@ import { menuArray } from './data.js'
 
 //menu element
 const menuDiv = document.getElementById('menu-container')
+const order = []
 
 render()
+
+
+//tracking the click
+document.addEventListener('click',function(e){
+    if(e.target.id === '0'){
+        console.log("Pizza")
+        handlePizzaOrder()
+    }else if(e.target.id === '1'){
+        console.log("Hamburger")
+    }else if(e.target.id === '2'){
+        console.log("Beer")
+    }
+})
+
+//handle the pizza
+function handlePizzaOrder(){
+    order.push({
+        id:0,
+        name:"Pizza",
+        price:14,
+    }) 
+    console.log(order)
+    genOrder()
+}
+
+function genOrder(){
+
+}
+
 
 //render the meun's products
 function getMenu(){
     let products = ''
     menuArray.forEach(function(product){
+        console.log(product.id)
         products+=`
-        <div class="menu" id="menu">
+        <div class="menu">
         <img class="menu-img" src="images/${product.name}.png" alt="pizza">
         <div class="container">
         <h3 class="product">${product.name}</h3>
@@ -19,7 +50,7 @@ function getMenu(){
         <h4 class="product-price">$${product.price}</h4>
         </div>
         <div class="btn-bg">
-            <i class="fa-solid fa-plus addProduct-Btn"></i>
+            <i class="fa-solid fa-plus addProduct-Btn" id=${product.id}></i>
         </div>
         </div>
         <hr>
